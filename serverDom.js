09e78dba3097ccb00139345179cbbfba9b615253
JackDan9 <1835812864@qq.com>
2018-05-11 10:33:07 +0800

    :cn:  jackdan : dom : node

--- serverDom.js
+++ serverDom.js
@@ -1,17 +1,46 @@
+const express = require("express");
+
+const app = express();
+
 const http = require('http');
+
+const server = http.createServer(app);
+
+const io = require("socket.io").listen(server);
 
 const cheerio = require('cheerio');
 
+app.use(express.static(__dirname));
+
+server.listen(8080);
+
 let url = 'http://www.ziroom.com/';
 
-http.get(url, function(res) {
-	let html = '';
-	let endHtml = ''
-	res.on('data', function(data) {
-		// console.log(data);
-		html += data;
-	}) 
-	res.on('end', function() {
-		console.log(html)
-	})
+let interval = -1;
+
+let objHtmlCon = http.get(url, function(res) {
+    let html = '';
+    res.on('data', function(data) {
+        html += data;
+    })
+    res.on('end', function() {
+        let objHtml = cheerio(html);
+        return objHtml;
+    })
+}).on('error', function() {
+    console.log('Get data wrong!');
+})
+
+io.sockets.on('connection', function () {
+    if (interval < 0) {
+        interval = setInterval(function () {
+            io.sockets.emit("domUpdate", {
+                objHtmlContent: objHtmlCon
+            });
+        }, 1000);
+    }
 });
+
+console.log("DomServer is Working!");
+
+
