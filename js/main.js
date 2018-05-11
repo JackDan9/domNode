5a9e5bf410a3fd0256439f398c567f8441dd5c38
JackDan9 <1835812864@qq.com>
2018-05-11 15:21:51 +0800

    :cn:  jackdan : dom : node

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 /**
  * Created by JackDan9 on 2018/5/11.
  */
+// import http from 'http'
+// import cherrio from 'cherrio'
+
 $(function () {
     let socket = io.connect("http://localhost:8080");
-    console.log(1);
     socket.on('domUpdate', function (update) {
-        // let obj = update.objHtmlContent;
-        // console.log(obj);
-        console.log(1);
+        let obj = update.objHtmlContent;
+        console.log(obj)
     });
 });
--- serverDom.js
+++ serverDom.js
@@ -6,6 +6,8 @@ const http = require('http');
 
 const server = http.createServer(app);
 
+// const server = require("http").createServer(app);
+
 const io = require("socket.io").listen(server);
 
 const cheerio = require('cheerio');
@@ -18,27 +20,71 @@ let url = 'http://www.ziroom.com/';
 
 let interval = -1;
 
-let objHtmlCon = http.get(url, function(res) {
-    let html = '';
-    res.on('data', function(data) {
-        html += data;
-    })
-    res.on('end', function() {
-        let objHtml = cheerio(html);
-        return objHtml;
-    })
-}).on('error', function() {
-    console.log('Get data wrong!');
-})
+// let objHtml = {};
+// let objHtml = '';
+
+// let objHtml = '';
+
+// function filterSlideList(html) {
+//     if (html) {
+//         var slideListData = [];
+//         slideListData.push(html);
+//         return slideListData;
+//     } else {
+//         console.log('无数据传入!')
+//     }
+// }
+//
+// function printInfo() {
+//
+// }
+
+// console.log(slideListData);
 
 io.sockets.on('connection', function () {
     if (interval < 0) {
-        interval = setInterval(function () {
-            io.sockets.emit("domUpdate", {
-                objHtmlContent: objHtmlCon
-            });
-        }, 1000);
+        // interval = setInterval(function () {
+            http.get(url, function(res) {
+                let html = '';
+                res.on('data', function(data) {
+                    html += data;
+                })
+                res.on('end', function() {
+                    let $ = cheerio.load(html); // JQuery Style
+                    let slideList = $('#foucsSlideList');
+                    let slideListData = [];
+                    slideList.find('li').each(function (item) {
+                        let pic = $(this);
+                        // console.log(pic)
+                        let pic_href = pic.find('a').attr('href');
+                        slideListData.push({
+                            pic_href: pic_href
+                        })
+                        // let con = $(this);
+                        // let conList = con.find('div');
+                        // console.log(conList)
+                        // slideListData.push({
+                        //     pic_href: pic_href
+                        // })
+                    })
+                    io.sockets.emit("domUpdate", {
+                        objHtmlContent: slideListData
+                    });
+                    // console.log(cheerio.load(html));
+                    // console.log(cheerio(html))
+                    // objHtml += cheerio(html);
+                    // return objHtml
+                    // sessionStorage.setItem('obj', objHtml);
+                    // var slideListData = filterSlideList(html);
+                    // printInfo(slideListData);
+                })
+            }).on('error', function() {
+                console.log('Get data wrong!');
+            })
+        // }, 100000);
     }
+    // let objHtmlContent = sessionStorage.getItem('obj');
+
 });
 
 console.log("DomServer is Working!");
